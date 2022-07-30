ec7eafc1041177ca03e0776b51f2b4efe78420eb
dragonnomada <dragonnomada123@gmail.com>
2022-07-29 19:08:42 -0500

    v1.0.0 r1

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/artesana",
+  base: "https://dragonnomada.github.io/artesana/",
   plugins: [vue()],
   resolve: {
     alias: {
